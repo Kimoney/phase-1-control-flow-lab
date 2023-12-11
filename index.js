3de40b5411491f222234790d56f9ccf431c15e30
John <johntaykimani@gmail.com>
2023-12-12 00:50:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(howFar){
   // Write your code here!
+  if (howFar <= 400){
+    return 'This one is on me!'
+  }
+  else if (howFar > 400 && howFar <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(whichCity){
   // Write your code here!
+  return whichCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tips){
   // Write your code here!
+  switch(tips){
+    case 'generous':
+      return 'Thank you so much.';
+      break
+    case 'not as generous':
+      return 'Thank you.';
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
